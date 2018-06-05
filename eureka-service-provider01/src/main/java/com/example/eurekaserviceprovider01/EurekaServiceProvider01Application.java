369c57e12e863bfa17477ef5ba22c0458840e646
HaitaoWang123 <1528779372@qq.com>
2018-06-05 11:01:14 +0800

    eureka-service-provider02

---
+++ eureka-service-provider01/src/main/java/com/example/eurekaserviceprovider01/EurekaServiceProvider01Application.java
@@ -0,0 +1,26 @@
+package com.example.eurekaserviceprovider01;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaServiceProvider01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceProvider01Application.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world ,port:" + port;
+    }
+}
